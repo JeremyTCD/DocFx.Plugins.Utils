d392cc55a6b9f976050bd2967e4c709ccc674d05
JeremyTCD <jeremytanchongdao@gmail.com>
2018-02-05 21:44:46 +0800

    Snippet now has same structure as an actual article
    - Consistent styles with full articles
    - Cleaner article list

--- src/JeremyTCD.DocFx.Plugins.Utils/SnippetCreator.cs
+++ src/JeremyTCD.DocFx.Plugins.Utils/SnippetCreator.cs
@@ -8,29 +8,51 @@ namespace JeremyTCD.DocFx.Plugins.Utils
         public static HtmlNode CreateSnippet(HtmlNode article, string href, int snippetLength)
         {
             HtmlNode snippet = HtmlNode.CreateNode("<article></article>");
-            snippet.AppendChildren(article.ChildNodes);
-            HtmlNode titleAnchorNode = HtmlNode.CreateNode($"<a href=\"/{href}\"></a>");
 
-            HtmlNode titleNode = snippet.SelectSingleNode(".//h1");
+            // Title
+            HtmlNode titleNode = article.SelectSingleNode(".//div[contains(@class, 'title')]");
             if (titleNode == null)
             {
-                throw new InvalidOperationException($"{nameof(SnippetCreator)}: Article {href} has no title (h1 element). A title is required for an article to " +
+                // Title node is used as link to the article, it is mandatory
+                throw new InvalidOperationException($"{nameof(SnippetCreator)}: Article {href} has no title (jtcd_pageTitle is unspecified). A title is required for an article to " +
                     $"be included in the article list.");
             }
+            HtmlNode titleAnchorNode = HtmlNode.CreateNode($"<a href=\"/{href}\"></a>");
             titleAnchorNode.InnerHtml = titleNode.InnerText;
-            titleNode.RemoveAllChildren();
-            titleNode.AppendChild(titleAnchorNode);
+            HtmlNode newTitleNode = titleNode.CloneNode(false);
+            newTitleNode.InnerHtml = "";
+            newTitleNode.AppendChild(titleAnchorNode);
+            snippet.AppendChild(newTitleNode);
 
-            TrimNode(snippet, 0, snippetLength);
-
-            HtmlNodeCollection headers = snippet.SelectNodes(".//*[self::h2 or self::h3 or self::h4 or self::h5 or self::h6]");
-            if (headers != null)
+            // Metadata
+            HtmlNode metaNode = article.SelectSingleNode(".//div[contains(@class, 'meta')]");
+            if(metaNode != null)
             {
-                foreach (HtmlNode node in headers)
-                {
-                    node.Attributes.Add("class", "no-anchor" + node.Attributes["class"]?.Value ?? "");
-                }
+                // If node is reused instead of cloned, article node will no longer be searcheable. Not sure why.
+                snippet.AppendChild(metaNode.CloneNode(true));
             }
+
+            // Content
+            HtmlNode contentNode = article.SelectSingleNode(".//div[contains(@class, 'content')]");
+            HtmlNode descriptionNode = contentNode.SelectSingleNode(".//p");
+            if(contentNode != null)
+            {
+                HtmlNode newContentNode = contentNode.CloneNode(false);
+                newContentNode.AppendChild(descriptionNode.CloneNode(true));
+                snippet.AppendChild(newContentNode.CloneNode(true));
+            }
+
+            // TODO allow user to specify that snippet should be the first x characters instead of the first paragraph of the article.
+            // TrimNode(snippet, 0, snippetLength);
+
+            //HtmlNodeCollection headers = snippet.SelectNodes(".//*[self::h2 or self::h3 or self::h4 or self::h5 or self::h6]");
+            //if (headers != null)
+            //{
+            //    foreach (HtmlNode node in headers)
+            //    {
+            //        node.Attributes.Add("class", "no-anchor" + node.Attributes["class"]?.Value ?? "");
+            //    }
+            //}
 
             return snippet;
         }
